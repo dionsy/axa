6a12e3e2429ef779190bb32d4072eb0a00cf5aad
WELE <aliou-samba.wele@atos.net>
2017-06-14 15:42:09 +0000

    slider

--- sites/all/themes/bootstrap/js/script.js
+++ sites/all/themes/bootstrap/js/script.js
@@ -1,1 +1,16 @@
-$('.slide').slick();
+/*(function($, Drupal){
+  Drupal.behaviors.kyziahSlickSlider = {
+    attach: function(context, settings){
+        $('.slide', context).slick({
+          autoplay: true,
+          dots: true,
+          infinite: true,
+          speed: 300,
+          slidesToShow: 1,
+        });}}}
+*/
+
+(function($) {
+    // Here $ is the jQuery namespace.
+    $('.slide').slick();
+})(jQuery);
