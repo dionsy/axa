d0f8126c28a79735ae1d7d7f865ef7e6c983daa4
dionsy <dionsady@yahoo.fr>
2017-06-19 11:24:23 +0000

    ajout message de confirmation sur page espace client

--- sites/axasite.dev.dd/settings.php
+++ sites/axasite.dev.dd/settings.php
@@ -244,8 +244,21 @@
  *   );
  * @endcode
  */
-$databases = array();
-
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'axa',
+      'username' => 'root',
+      'password' => '@tosdev',
+      'host' => '10.94.216.37',
+      'port' => '3306',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 /**
  * Access control for update.php script.
  *
