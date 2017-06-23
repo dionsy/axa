547b26e2d8a57433903ef76dc244fbd9ac638d47
SYLL <ababacar.syll@atos.net>
2017-06-23 12:24:50 +0000

    css

--- sites/axasite.dev.dd/settings.php
+++ sites/axasite.dev.dd/settings.php
@@ -259,6 +259,25 @@ $databases = array (
     ),
   ),
 );
+
+
+
+/*$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'axa',
+      'username' => 'root',
+      'password' => '',
+      'host' => 'localhost',
+      'port' => '3306',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);*/
+
 /**
  * Access control for update.php script.
  *
