94456d9dd350051e9a1e5fde9320172dc3551d70
WELE <aliou-samba.wele@atos.net>
2017-06-30 16:21:36 +0000

    notification sms

--- sites/all/libraries/twilio/Tests/Bootstrap.php
+++ sites/all/libraries/twilio/Tests/Bootstrap.php
@@ -4,21 +4,24 @@ error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 1);
 
 $root = realpath(dirname(dirname(__FILE__)));
-$library = "$root/Rest";
-$tests = "$root/Tests";
+$library = "$root/Services";
+$tests = "$root/tests";
 
 $path = array($library, $tests, get_include_path());
 set_include_path(implode(PATH_SEPARATOR, $path));
 
-$vendorFilename = dirname(__FILE__) . '/../../vendor/autoload.php';
+$vendorFilename = dirname(__FILE__) . '/../vendor/autoload.php';
 if (file_exists($vendorFilename)) {
     /* composer install */
-    /** @noinspection PhpIncludeInspection */
     require $vendorFilename;
+} else {
+    /* hope you have it installed somewhere. */
+    require_once 'Mockery/Loader.php';
 }
+$loader = new \Mockery\Loader;
+$loader->register();
 
-/** @noinspection PhpIncludeInspection */
-require_once 'Client.php';
+require_once 'Twilio.php';
 
 unset($root, $library, $tests, $path);
 
