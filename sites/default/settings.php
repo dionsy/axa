9f83433313d05e5587531a9658cb171cd166957d
dionsy <dionsady@yahoo.fr>
2017-07-03 16:44:34 +0000

    settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -245,23 +245,24 @@
  *   );
  * @endcode
  */
-// $databases = array (
-//   'default' => 
-//   array (
-//     'default' => 
-//     array (
-//       'database' => 'axa',
-//       'username' => 'root',
-//       'password' => '@tosdev',
-//       'host' => '10.94.216.37',
-//       'port' => '3306',
-//       'driver' => 'mysql',
-//       'prefix' => '',
-//     ),
-//   ),
-// );
-
 $databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'axa',
+      'username' => 'root',
+      'password' => '@tosdev',
+      'host' => '10.94.216.37',
+      'port' => '3306',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
+
+
+/*$databases = array (
   'default' => 
   array (
     'default' => 
@@ -275,7 +276,7 @@ $databases = array (
       'prefix' => '',
     ),
   ),
-);
+);*/
 
 /**
  * Access control for update.php script.
