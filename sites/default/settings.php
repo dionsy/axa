d466c973951f50471672294270692afecfa547a0
SYLL <ababacar.syll@atos.net>
2017-07-03 09:36:45 +0000

    css et template

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -245,22 +245,6 @@
  *   );
  * @endcode
  */
-// $databases = array (
-//   'default' => 
-//   array (
-//     'default' => 
-//     array (
-//       'database' => 'axa',
-//       'username' => 'root',
-//       'password' => '@tosdev',
-//       'host' => '10.94.216.37',
-//       'port' => '3306',
-//       'driver' => 'mysql',
-//       'prefix' => '',
-//     ),
-//   ),
-// );
-
 $databases = array (
   'default' => 
   array (
@@ -276,6 +260,22 @@ $databases = array (
     ),
   ),
 );
+
+/*$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'axa',
+      'username' => 'root',
+      'password' => '',
+      'host' => 'localhost',
+      'port' => '3306',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);*/
 
 /**
  * Access control for update.php script.
