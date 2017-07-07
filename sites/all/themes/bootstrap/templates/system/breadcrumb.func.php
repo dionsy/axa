79fed74cf584c45d2d54bd578e0d12fe68ef4f18
dionsy <dionsady@yahoo.fr>
2017-07-07 12:28:16 +0000

    breadcrumb

--- sites/all/themes/bootstrap/templates/system/breadcrumb.func.php
+++ sites/all/themes/bootstrap/templates/system/breadcrumb.func.php
@@ -20,23 +20,60 @@
  */
 function bootstrap_breadcrumb($variables) {
   // Use the Path Breadcrumbs theme function if it should be used instead.
-  if (_bootstrap_use_path_breadcrumbs()) {
+  
+ /* if (_bootstrap_use_path_breadcrumbs()) {
     return path_breadcrumbs_breadcrumb($variables);
+  }*/
+
+  $trail = menu_get_active_trail();
+  $output='';
+
+  if(isset($trail[1]['map'])){
+
+     $node = $trail[1]['map'][1];
+     $type= $node->type;
+     $tid= $node->field_type_de_contrat['und'][0]['tid'];
+     $taxo = taxonomy_term_load($tid);
+     $type_contrat = $taxo->name;
+
+    switch ($type) {
+      case 'nos_produits':
+        $terms = field_view_field('node', $node, 'field_type_de_contrat');  
+        $variables['breadcrumb']['1'] = '<span>Assurance</span>';
+        $variables['breadcrumb']['2'] = '<span>'.$terms['0']['#title'].'</span>';
+        break;     
+    }
   }
 
-  $output = '';
   $breadcrumb = $variables['breadcrumb'];
+  $crumb_arrow = '';
+  $arr_crumbs = array();
 
-  // Determine if we are to display the breadcrumb.
-  $bootstrap_breadcrumb = bootstrap_setting('breadcrumb');
-  if (($bootstrap_breadcrumb == 1 || ($bootstrap_breadcrumb == 2 && arg(0) == 'admin')) && !empty($breadcrumb)) {
-    $output = theme('item_list', array(
-      'attributes' => array(
-        'class' => array('breadcrumb'),
-      ),
-      'items' => $breadcrumb,
-      'type' => 'ol',
-    ));
+  if (!empty($breadcrumb)) {
+
+
+    array_push($arr_crumbs,  implode($crumb_arrow, $breadcrumb));
+
+    $array_size = count($arr_crumbs);
+    for ($i=0; $i < $array_size; $i++) {
+      if ( $i == $array_size - 1 ) {
+        // Menu link title may override the content title
+        (menu_get_active_title()) ? $current_crumb = menu_get_active_title() : $current_crumb = drupal_get_title();
+        // If current page is 'Edit Page'
+        if (substr(drupal_get_title(), 0, 18) == '<em>Edit Page</em>') {
+          $current_crumb = 'Edit';
+        }
+
+
+        $output .= $arr_crumbs[$i] . '<span class="crumbs-current a--breadcrumb">' . $current_crumb . '</span>';
+       break;
+      }
+      if($arr_crumbs[$i]=="array")
+     // krumong('main')->kPrint($arr_crumbs[$i]);
+      $output .= $arr_crumbs[$i];    
+    }
+
+
+    return '<div class="breadcrumb small clearfix">' . $output . '</div>';
   }
-  return $output;
 }
