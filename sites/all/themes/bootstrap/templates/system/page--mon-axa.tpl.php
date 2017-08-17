6be62af3ad299c144cab613c786cb2554de308d8
dionsy <dionsady@yahoo.fr>
2017-08-17 10:10:58 +0000

    mon axa

--- sites/all/themes/bootstrap/templates/system/page--mon-axa.tpl.php
+++ sites/all/themes/bootstrap/templates/system/page--mon-axa.tpl.php
@@ -23,24 +23,8 @@
 
   <div class="main-container "> 
      <div class="row">
-         <section id="test" class="container">
-            
+         <section id="test" class="container">            
             <a id="main-content"></a>
-            <?php print render($title_prefix); ?>
-            <?php if (!empty($title)): ?>
-              <h1 class="page-header"><?php print $title; ?></h1>
-            <?php endif; ?>
-            <?php print render($title_suffix); ?>
-            <?php print $messages; ?>
-            <?php if (!empty($tabs)): ?>
-              <?php print render($tabs); ?>
-            <?php endif; ?>
-            <?php if (!empty($page['help'])): ?>
-              <?php print render($page['help']); ?>
-            <?php endif; ?>
-            <?php if (!empty($action_links)): ?>
-              <ul class="action-links"><?php print render($action_links); ?></ul>
-            <?php endif; ?>
             <?php print render($page['content']); ?>
           </section>
       </div>            
