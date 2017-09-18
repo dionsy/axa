65bfbc514ae060598701950ab8325c5ec284a0e2
SYLL <ababacar.syll@atos.net>
2017-09-18 16:26:29 +0000

    entreprise

--- sites/all/themes/bootstrap/templates/block/block.tpl.php
+++ sites/all/themes/bootstrap/templates/block/block.tpl.php
@@ -167,6 +167,9 @@ switch ($block->delta) {
 	case 'menu-menu-principal-professionne':	
 		$class="col-md-10 col-lg-10 col-sm-12 col-xs-12 responsive-menus-mean-menu-processed";
 	break;
+	case 'menu-menu-principal-entreprise':	
+		$class="col-md-10 col-lg-10 col-sm-12 col-xs-12 responsive-menus-mean-menu-processed";
+	break;
 
 
 	case '3':
